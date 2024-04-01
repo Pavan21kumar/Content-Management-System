965bfede2a4e948a2a6d7f34c160057c13679bf7
Pavan21Kumar <budurupavankumar268@gmail.com>
2024-04-01 10:07:08 +0530

    debug the Create BlogPost Feature

--- src/main/java/com/example/cms/serviceimpl/BlogPostServiceImpl.java
+++ src/main/java/com/example/cms/serviceimpl/BlogPostServiceImpl.java
@@ -1,18 +1,23 @@
 package com.example.cms.serviceimpl;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.example.cms.dto.BlogPostRequest;
 import com.example.cms.dto.BlogPostResponse;
 import com.example.cms.dto.BlogResponse;
 import com.example.cms.entity.BlogPost;
+import com.example.cms.entity.Users;
 import com.example.cms.enums.PostType;
 import com.example.cms.repository.BlogPostRepository;
 import com.example.cms.repository.BlogsRepository;
 import com.example.cms.service.BlogPostService;
 import com.example.cms.util.BlogNotFoundException;
+import com.example.cms.util.IllegalAccessRequestException;
 import com.example.cms.util.Responstructure;
 
 import lombok.AllArgsConstructor;
@@ -27,12 +32,18 @@ public class BlogPostServiceImpl implements BlogPostService {
 
 	@Override
 	public ResponseEntity<Responstructure<BlogPostResponse>> createDraft(BlogPostRequest postRequest, int blogId) {
-
+		String email = SecurityContextHolder.getContext().getAuthentication().getName();
 		return blogRepo.findById(blogId).map(blog -> {
+			List<Users> users = blog.getPanel().getUsers();
+			for (Users user : users) {
+				if (user.getEmail().equals(email) != false)
+					throw new IllegalAccessRequestException("Login User Not A Contributer...");
+			}
 			BlogPost blogPost = maptoBlogPost(postRequest);
 			blogPost = postRepo.save(blogPost);
 			return ResponseEntity.ok(response.setStatusCode(HttpStatus.OK.value()).setMessage("post is created")
 					.setData(mapToResponse(blogPost)));
+
 		}).orElseThrow(() -> new BlogNotFoundException("blog not found by Given Id"));
 	}
 
